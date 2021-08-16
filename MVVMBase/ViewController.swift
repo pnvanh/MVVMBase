6c1826ca32d5501896445984cd85cb49a2fd3ea8
Anh Phan <pnvanh.dev@gmail.com>
2021-08-16 16:07:15 +0700

    Initial Commit

---
+++ MVVMBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMBase
+//
+//  Created by Viet Anh on 16/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
